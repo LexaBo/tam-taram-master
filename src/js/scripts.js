cbbb06a17627f53995445aedd1bdce7c5ce95e63
LexaBo <32167787+LexaBo@users.noreply.github.com>
2018-08-31 19:58:52 +0300

    Add button play

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -57,7 +57,7 @@ const slick = {
                 {
                     breakpoint: 768,
                     settings:'slick'
-                }
+                },
             ]
         });
     },
