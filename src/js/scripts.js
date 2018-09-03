272277d77de66ccaee9ee8ac7e9895a0646ca9e0
LexaBo <32167787+LexaBo@users.noreply.github.com>
2018-09-03 14:40:35 +0300

    Fix svg

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,28 +1,4 @@
-function TakeImgTransformToSvg(){
-    jQuery('img.svg').each(function(){
-        const $img = jQuery(this);
-        const imgID = $img.attr('id');
-        const imgClass = $img.attr('class');
-        const imgURL = $img.attr('src');
 
-        jQuery.get(imgURL, function(data) {
-            let $svg = jQuery(data).find('svg');
-
-            if(typeof imgID !== 'undefined') {
-                $svg = $svg.attr('id', imgID);
-            }
-            if(typeof imgClass !== 'undefined') {
-                $svg = $svg.attr('class', imgClass+' replaced-svg');
-            }
-
-            $svg = $svg.removeAttr('xmlns:a');
-
-            $img.replaceWith($svg);
-
-        }, 'xml');
-
-    });
-}
 const slick = {
     init: function() {
         this.toggleSliders();
@@ -77,7 +53,6 @@ const slick = {
 
 $(document).ready(function () {
     slick.init();
-    TakeImgTransformToSvg();
 });
 
 $('.menu').on('click', function() {
