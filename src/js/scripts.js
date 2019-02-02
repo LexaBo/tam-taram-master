e95e01d3bec449227eeb428ee93ac7a6522d7e59
LexaBo <32167787+LexaBo@users.noreply.github.com>
2019-02-02 16:45:55 +0300

    Fix button load more

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,32 +1,44 @@
+const main = document.body.querySelector('main');
+main.addEventListener('click', function (e) {
+    if (e.target.classList.contains('button')) {
+        const id = e.target.getAttribute('data-id');
+        const children = document.getElementById(`${id}`).children;
+        for (let i = 0; i < children.length; i++) {
+            children[i].classList.remove('video_hide');
+        }
+        e.target.style.display = 'none';
+    }
+});
+
 
 const slick = {
-    init: function() {
+    init: function () {
         this.toggleSliders();
         this.events();
 
     },
 
-    events: function() {
-        $(window).on('resize', function() {
+    events: function () {
+        $(window).on('resize', function () {
             slick.toggleSliders();
         });
     },
-    slickInit: function() {
+    slickInit: function () {
         $('.thumb-wrap').not('.slick-initialized').slick({
             infinite: true,
-            arrows:true,
-            prevArrow:'<span class="slick-prev"><img class="arrow" src="../img/ico_dropdown_grey.svg" alt=""></span>',
-            nextArrow:'<span class="slick-next"><img class="arrows" src="../img/ico_dropdown_grey.svg" alt=""></span>',
+            arrows: true,
+            prevArrow: '<span class="slick-prev"><img class="arrow" src="../img/ico_dropdown_grey.svg" alt=""></span>',
+            nextArrow: '<span class="slick-next"><img class="arrows" src="../img/ico_dropdown_grey.svg" alt=""></span>',
             mobileFirst: true,
             responsive: [
                 {
                     breakpoint: 320,
-                    settings:'unslick'
+                    settings: 'unslick'
                 },
 
                 {
                     breakpoint: 768,
-                    settings:{
+                    settings: {
                         slidesToShow: 2,
                         slidesToScroll: 1,
                     }
@@ -34,7 +46,7 @@ const slick = {
 
                 {
                     breakpoint: 1145,
-                    settings:{
+                    settings: {
                         slidesToShow: 3,
                         slidesToScroll: 1,
                     }
@@ -55,10 +67,11 @@ $(document).ready(function () {
     slick.init();
 });
 
-$('.menu').on('click', function() {
-    $('.navigation').slideToggle(300, function(){
-        if( $(this).css('display') === 'none'){
+$('.menu').on('click', function () {
+    $('.navigation').slideToggle(300, function () {
+        if ($(this).css('display') === 'none') {
             $(this).removeAttr('style');
         }
     });
 });
+
